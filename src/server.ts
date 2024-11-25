34a0f65710752467e97673296c6fbcaf4815ec39
Ahsan Tonmoy <ahosant82@gmail.com>
2024-11-25 15:30:22 +0600

    server is on

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-
+app.get('/', (req, res) => {
+  res.send('bike store server runing')
+})
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
