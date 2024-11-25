cca92dd73d0c96d5476c4174369c7385d1e87c35
Ahsan Tonmoy <ahosant82@gmail.com>
2024-11-25 16:20:52 +0600

    Fixd issue for Vercel deployment

--- src/models/api/index.ts
+++ src/models/api/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import bodyParser from "body-parser";
-import bikeRoutes from "../src/routes/bikes";
-import connectDB from "../src/config/db";
+import bikeRoutes from "../../routes/bikes";
+import connectDB from "../../config/db";
 
 // Initialize app and connect to DB
 connectDB();
